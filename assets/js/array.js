c3c40ce2f508e82282ea19471cb7737b7c09f58e
mariadelcfernandez <kennel.liebig@gmail.com>
2024-05-08 18:03:07 -0300

    Actualizando index

---
+++ assets/js/array.js
@@ -0,0 +1,12 @@
+const var1 = 'asdfg'
+const var2 =2
+const var3 = 10
+
+const array =['asdfg',2, 10]
+for(let i=0; i <=3;i++){
+    console.log(i)
+}
+const categorias=['a','b','c']
+for(let i=0; i<=categorias.length ;i++){
+    console.log(i)
+}
