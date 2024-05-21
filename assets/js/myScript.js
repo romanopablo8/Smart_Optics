8d148ec02372b4487de7827d5e9cef977899cf7d
Mary <kennel.liebig@gmail.com>
2024-05-21 02:37:39 -0300

    Validaciones actualizadas

--- assets/js/myScript.js
+++ assets/js/myScript.js
@@ -1,37 +1,91 @@
-console.log(123)
-console.log("123")
-let var1 = 'abc'
-let var2 = 123
-console.log({var1})
-console.log( {var2})
-console.log( typeof var1)
-console.log( typeof var2)
-const var3 = (prompt('Digame un numero'))
 
-//console.log( {var3})
-//let var4 = Number ='1234'
-//console.log( {var4})
-//const var5 =12
- //var5 =-Infinity
-//let var6 = Number.isInteger(var5)
-//console.log({var6})
-const var8 =20
-const var9 = 30
 
-//const resultado = var8 ===var9
-//if(resultado){
-  //  console.log('Estoy dentro del IF')
-
-//}else{ 
-   // console.log('Estoy saliendo poe el else')
-//}
-//console.log('Fin')
-
-// False
-if(var8 === var9 && var8===20){
-    console.log('Estoy dentro del IF')
-
-}else{ 
-    console.log('Estoy saliendo poe el else')
+function validar(f) {
+  const mensajeError = document.getElementById('mensajeError');
+var todo_correcto = true;
+// Validación de los campos
+if(document.getElementById('nombre').value.length < 2 ){
+  mensajeError.innerText ='El campo nombre no puede estar vacío.';
+  todo_correcto = false;
 }
-console.log('Fin')
+if(document.getElementById('password').value.length < 10 ){
+  mensajeError.innerText ='El campo nombre no puede estar vacío.';
+  todo_correcto = false;
+}
+if(!todo_correcto){
+  alert('Algunos campos no están correctos, vuelva a revisarlos');
+}
+else {
+  alert("Usuario logueado");
+return todo_correcto;
+
+}
+}
+//el formulario se envia
+
+function registrar(f) {
+  const mensajeError = document.getElementById('mensajeError');
+var todo_correcto = true;
+// Validación de los campos
+if(document.getElementById('nombre').value.length < 2 ){
+  mensajeError.innerText ='El campo nombre no puede estar vacío.';
+  todo_correcto = false;
+}
+if(document.getElementById('apellido').value.length < 2 ){
+  mensajeError.innerText ='El campo nombre no puede estar vacío.';
+  todo_correcto = false;
+}
+if(document.getElementById('email').value.length < 10 ){ 
+  mensajeError.innerText ='El campo nombre no puede estar vacío.';
+  todo_correcto = false;
+  
+}
+if(document.getElementById('password').value.length < 10 ){
+  mensajeError.innerText ='El campo nombre no puede estar vacío.';
+  todo_correcto = false;
+}
+if(!todo_correcto){
+  alert('Algunos campos no están correctos, vuelva a revisarlos');
+}
+else {
+  alert("Usuario registrado");
+  setTimeout(function() {w.close();}, 6000);
+  let isBoss = confirm("¿Loguearse?");
+  alert(location.href ="https://romanopablo8.github.io/Smart_Optics/login.html");
+  
+return todo_correcto;
+
+}
+}
+//el formulario se envia
+
+
+function mensajear(a) {
+  const mensajeError = document.getElementById('mensajeError');
+  var todo_correcto = true;
+// Validación de los campos
+if(document.getElementById('nombre').value.length < 2 ){
+  mensajeError.innerText ='El campo nombre no puede estar vacío.';
+  todo_correcto = false;
+}
+
+if(document.getElementById('email').value.length < 10 ){ 
+  mensajeError.innerText ='El campo nombre no puede estar vacío.';
+  todo_correcto = false;
+  
+}
+if(!todo_correcto){
+  alert('Algunos campos no están correctos, vuelva a revisarlos');
+}
+else {
+  alert("Mensaje enviado. Le contestaremos en breve. Gracias");
+  return todo_correcto;
+}
+}
+//el formulario se envia
+
+
+
+
+
+
