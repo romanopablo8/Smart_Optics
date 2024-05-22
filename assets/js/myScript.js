07c14d5d9b673e88efb8b6fb60f500a9889aa2e2
Mary <kennel.liebig@gmail.com>
2024-05-22 03:08:08 -0300

    Actualizar responsive

--- assets/js/myScript.js
+++ assets/js/myScript.js
@@ -1,38 +1,47 @@
 
 
 function validar(f) {
-  const mensajeError = document.getElementById('mensajeError');
+const mensajeError = document.getElementById('mensajeError');
+var terminos = document.getElementById('terminos');
 var todo_correcto = true;
 // Validación de los campos
+
 if(document.getElementById('nombre').value.length < 2 ){
-  mensajeError.innerText ='El campo nombre no puede estar vacío.';
+  mensajeError.innerText ='El nombre de usurio es inválido.';
   todo_correcto = false;
 }
-if(document.getElementById('password').value.length < 10 ){
-  mensajeError.innerText ='El campo nombre no puede estar vacío.';
+if(document.getElementById('password').value.length < 8 ){
+  mensajeError.innerText ='El password  es inválido.';
   todo_correcto = false;
 }
 if(!todo_correcto){
   alert('Algunos campos no están correctos, vuelva a revisarlos');
 }
 else {
-  alert("Usuario logueado");
+  alert ("Usuario logueado");
+  setTimeout(function() {w.close();}, 2000);
+  let isBoss = confirm('Bienvenido');
+  console.log(isBoss);
+ window.location.href="https://romanopablo8.github.io/Smart_Optics/productos.html";
 return todo_correcto;
 
 }
 }
 //el formulario se envia
 
+///Validacion del Formulario de Registrar
 function registrar(f) {
   const mensajeError = document.getElementById('mensajeError');
 var todo_correcto = true;
+var password = document.getElementById("password").value;
+var password1 = document.getElementById("password1").value;
 // Validación de los campos
 if(document.getElementById('nombre').value.length < 2 ){
   mensajeError.innerText ='El campo nombre no puede estar vacío.';
   todo_correcto = false;
 }
 if(document.getElementById('apellido').value.length < 2 ){
-  mensajeError.innerText ='El campo nombre no puede estar vacío.';
+  mensajeError.innerText ='El campo apellido no puede estar vacío.';
   todo_correcto = false;
 }
 if(document.getElementById('email').value.length < 10 ){ 
@@ -40,26 +49,41 @@ if(document.getElementById('email').value.length < 10 ){
   todo_correcto = false;
   
 }
-if(document.getElementById('password').value.length < 10 ){
+
+if(document.getElementById('password').value.length < 8 ){
   mensajeError.innerText ='El campo nombre no puede estar vacío.';
   todo_correcto = false;
 }
+if (password !== password1) {
+  document.getElementById("mensajeError").textContent = "Las contraseñas no coinciden.";
+  todo_correcto= false;
+}
+/*if(document.getElementById('password1').value.length <8 ){
+  mensajeError.innerText ='Las claves deben coincidir';
+  todo_correcto = false;
+}*/
+if (!terminos) {
+  mensajeError.innerText = 'Debes aceptar los términos y condiciones.';
+  todo_correcto = false;
+}
+
 if(!todo_correcto){
   alert('Algunos campos no están correctos, vuelva a revisarlos');
 }
 else {
   alert("Usuario registrado");
   setTimeout(function() {w.close();}, 6000);
-  let isBoss = confirm("¿Loguearse?");
-  alert(location.href ="https://romanopablo8.github.io/Smart_Optics/login.html");
-  
+  let isBoss = confirm("¿Desea Loguearse?");
+  if (isBoss == true){
+  window.location.href ="https://romanopablo8.github.io/Smart_Optics/login.html";
+  }
 return todo_correcto;
-
 }
+
 }
 //el formulario se envia
 
-
+// Validacion de Formulario de contacto
 function mensajear(a) {
   const mensajeError = document.getElementById('mensajeError');
   var todo_correcto = true;
@@ -70,10 +94,15 @@ if(document.getElementById('nombre').value.length < 2 ){
 }
 
 if(document.getElementById('email').value.length < 10 ){ 
-  mensajeError.innerText ='El campo nombre no puede estar vacío.';
+  mensajeError.innerText ='El campo email no puede estar vacío.';
   todo_correcto = false;
-  
+
 }
+if(document.getElementById('mensaje').value.length < 1 ){
+  mensajeError.innerText ='El campo mensaje no puede estar vacío.';
+  todo_correcto = false;
+}
+
 if(!todo_correcto){
   alert('Algunos campos no están correctos, vuelva a revisarlos');
 }
